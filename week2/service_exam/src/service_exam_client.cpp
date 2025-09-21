0288c583fa2ac791f751b400c72ddf3d4b82a7d5
Revivekirin <kimwiz4352@gmail.com>
2025-09-21 12:14:41 +0900

    service_exam baseline

--- week2/service_exam/src/service_exam_client.cpp
+++ week2/service_exam/src/service_exam_client.cpp
@@ -1,3 +1,45 @@
 #include "rclcpp/rclcpp.hpp"
-#include "service_exam/myservice.h"
+#include "service_exam/srv/myservice.hpp"
 #include <cstdlib>
+#include <memory>
+#include <chrono>
+
+using namespace std::chrono_literals;
+using Myservice = service_exam::srv::myservice;
+
+int main(int argc, char **argv)
+{
+    rclcpp::init(argc, argv);
+    auto node = rclcpp::Node::make_shared("service_exam_client");
+
+    if (argc != 3) {
+        RCLCPP_INFO(node->get_logger(), "usage: service_exam_client num1 num2");
+        return 1;
+    }
+    auto client = node->create_client<Myservice>("myservice")
+
+    while (!client->wait_for_service(1s)) {
+        if (!rclcpp::ok()) {
+            RCLCPP_ERROR(node->get_logger(), "Interrupted while waiting for the service.");
+            return 1;
+        }
+        RCLCPP_INFO(node->get_logger(), "service not available, waiting...");
+    }
+
+    auto request = std::make_shared<Myservice::Request>();
+    request->a = std::atoll(argv[1]);
+    request->b = std::atoll(argv[2]);
+
+    auto future = client->async_send_request(request);
+
+    auto ret = rclcpp::spin_unil_future_complete(node, future);
+    if (ret == rclcpp::FutureReturnCode::SUCESS) {
+        RCLCPP_INFO(node->get_logger(), "sum: %ld", static_cast<long>(future.get()->result));
+    } else [
+        RCLCPP_ERROR(node->get_logger(), "Failed to call service my_service");
+        return 1;
+    ]
+
+    rclcpp::shutdown();
+    return 0;
+}
--- week2/service_exam/src/service_exam_server.cpp
+++ week2/service_exam/src/service_exam_server.cpp
@@ -1,3 +1,37 @@
+#include <memory>
 #include "rclcpp/rclcpp.hpp"
-#include "service_exam/myservice.h"
+#include "service_exam/srv/myservice.hpp"
 
+using MyService = service_exam::srv::myservice;
+
+class ServiceExamServer : public rclcpp::Node {
+public:
+    ServiceExamServer() : rclcpp::Node("service_exam_server") {
+        service_ = this->create_service<MyService>(
+            "myservice",
+            std::bind(&ServiceExamServer::handle_service, this,
+            std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+        RCLCPP_INFO(this->get_logger(), "my_service: ready");
+    }
+private:
+  void handle_service(
+    const std::shared_ptr<rmw_request_id_t>,
+    const std::shared_ptr<MyService::Request> req,
+    std::shared_ptr<MyService::Response> res
+  ) {
+    res->result = req->a + req->b;
+    RCLCPP_INFO(this->get_logger(), "request: a=%ld, b=%ld",
+                static_cast<long>(req->a), static_cast<long>(req->b));
+    RCLCPP_INFO(this->get_logger(), "sending back response [%ld]",
+                static_cast<long>(res->result));
+  }
+
+  rclcpp::Service<MyService>::SharedPtr servicee_;
+};
+
+int mian(int argc, char ** argv) {
+    rclcpp::init(argc, argv);
+    rclcpp::spin(std::make_shared<ServiceExamServer>());
+    rclcpp::shutdown();
+    return 0;
+}
