773bd85f0b0853cfa35e87e7219faca339607f11
Revivekirin <kimwiz4352@gmail.com>
2025-09-23 08:51:26 -0700

    Complete action_exam

--- week2/action_exam/src/action_exam_client.cpp
+++ week2/action_exam/src/action_exam_client.cpp
@@ -2,67 +2,82 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "rclcpp_action/rclcpp_action.hpp"
-#include "action_exam/action/Primenumber.hpp"
+#include "action_exam/action/primenumber.hpp"
 
 using namespace std::chrono_literals;
 
 class PrimenumberActionClient : public rclcpp::Node
 {
 public:
-  using Primenumber = action_exam::action::Primenumber;
-  using GoalHandlePrimenumber = rclcpp_action::ClientGoalHandle<Primenumber>;
+  using ActionT = action_exam::action::Primenumber;
+  using GoalHandleT = rclcpp_action::ClientGoalHandle<ActionT>;
 
-  PrimenumberActionClient()
-  : rclcpp::Node("action_exam_client")
-  {
-    client_ = rclcpp_action::create_client<Primenumber>(this, "action_exam_action");
+  PrimenumberActionClient() : rclcpp::Node("action_exam_client") {
+    client_ = rclcpp_action::create_client<ActionT>(this, "action_exam_action");
   }
 
-  void send_goal(int target)
-  {
+  void send_goal(int target) {
     if (!client_->wait_for_action_server(5s)) {
-        RCLCPP_ERROR(this->get_logger(), "Action server not available");
-        return;
+      RCLCPP_ERROR(this->get_logger(), "Action server not available");
+      return;
     }
 
-    Primenumber::Goal goal_msg;
-    goal_msg.target = target;
+    ActionT::Goal goal;
+    goal.target = target;
 
-    rclcpp_action::Client<Primenumber>::SendGoalOptions opts;
-    opts.goal_response_callback = 
-      [this](std::shared_ptr<GoalHandlePrimenumber> handle) {
-        if (!handle) {
+    rclcpp_action::Client<ActionT>::SendGoalOptions opts;
+
+    // ★ Foxy 요구 시그니처: shared_future 인자로 받아야 함
+    opts.goal_response_callback =
+      [this](std::shared_future<typename GoalHandleT::SharedPtr> future) {
+        auto goal_handle = future.get();
+        if (!goal_handle) {
           RCLCPP_ERROR(this->get_logger(), "Goal rejected");
         } else {
-            RCLCPP_INFO(this->get_logger(), "Goal accepted");
+          RCLCPP_INFO(this->get_logger(), "Goal accepted");
         }
-    };
+      };
 
-    opts.feedback_callback = 
-      [this](GoalHandlePrimenumber::SharedPtr,
-             const std::shared_ptr<const Primenumber::Feedback> feedback) {
-                RCLCPP_INFO(this->get_logger(), "Feedback: cur_num=%d", feedback->cur_num);
-             };
-    
-    opts.result_callback = 
-      [this](const GoalHandlePrimenumber::WrappedResult & result) {
+    opts.feedback_callback =
+      [this](GoalHandleT::SharedPtr,
+             const std::shared_ptr<const ActionT::Feedback> feedback) {
+        RCLCPP_INFO(this->get_logger(), "Feedback: cur_num=%d", feedback->cur_num);
+      };
+
+    opts.result_callback =
+      [this](const GoalHandleT::WrappedResult & result) {
         using rclcpp_action::ResultCode;
-        if(result.code == ResultCode::SUCCEEDED) {
-            RCLCPP_INFO(this->get_logger(), "succeeded, %zu primes:",
-                        result.result->sequence.size());
-            for (size_t i=0; i< result.result->sequence.size(); ++i) {
-                RCLCPP_INFO(this->get_logger(), "%zu: %d", i+1, result.result->sequence[i]);
-            }
+        if (result.code == ResultCode::SUCCEEDED) {
+          RCLCPP_INFO(this->get_logger(), "Succeeded. %zu primes:",
+                      result.result->sequence.size());
+          for (size_t i = 0; i < result.result->sequence.size(); ++i) {
+            RCLCPP_INFO(this->get_logger(), "%zu: %d", i + 1, result.result->sequence[i]);
+          }
         } else if (result.code == ResultCode::CANCELED) {
-            RCLCPP_WARN(this->get_logger(), "Canceled");
+          RCLCPP_WARN(this->get_logger(), "Canceled");
         } else {
-            RCLCPP_ERROR(this->get_logger(), "Aborted");
+          RCLCPP_ERROR(this->get_logger(), "Aborted");
         }
         rclcpp::shutdown();
       };
-      
-    client_->async_send_goal(goal_msg, opts);
 
-    rclcpp::spin(node);
-    return 0;
+    client_->async_send_goal(goal, opts);
   }
+
+private:
+  rclcpp_action::Client<ActionT>::SharedPtr client_;
+};
+
+int main(int argc, char ** argv)
+{
+  rclcpp::init(argc, argv);
+  auto node = std::make_shared<PrimenumberActionClient>();
+
+  int target = 20;
+  if (argc == 2) target = std::atoi(argv[1]);
+
+  node->send_goal(target);
+  rclcpp::spin(node);
+  return 0;
+}
+
--- week2/action_exam/src/action_exam_server.cpp
+++ week2/action_exam/src/action_exam_server.cpp
@@ -1,14 +1,13 @@
-#include "rclcpp/rclcpp.hpp"
-#include "rclcpp_action/rclcpp_action.hpp"
-#include "action_exam/action/Primenumber.hpp"
 #include <memory>
 #include <thread>
 #include <chrono>
+#include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "action_exam/action/primenumber.hpp"
 
 using namespace std::chrono_literals;
 
-
-class PrimenumberAction : public rclcpp::Node
+class PrimenumberActionServer : public rclcpp::Node
 {
 public:
   using Primenumber = action_exam::action::Primenumber;
@@ -20,82 +19,84 @@ public:
     using std::placeholders::_1;
     using std::placeholders::_2;
 
-    server = rlcpp_action::create_server<Primenumber>(
-        this,
-        "action_exam_action",
-        std::bind(&PrimenumberActionServer::handle_goal, this, _1, _2),
-        std::bind(&PrimenumberActionServer::handle_cancel, this, _1),
-        std::bind(&PrimenumberActionServer::handle_accepted, this, _1)
-    );
+    server_ = rclcpp_action::create_server<Primenumber>(
+      this,
+      "action_exam_action",  // 액션 이름 (클라와 동일해야 함)
+      std::bind(&PrimenumberActionServer::handle_goal, this, _1, _2),
+      std::bind(&PrimenumberActionServer::handle_cancel, this, _1),
+      std::bind(&PrimenumberActionServer::handle_accepted, this, _1));
+
     RCLCPP_INFO(this->get_logger(), "action_exam_action: ready");
   }
 
 private:
   rclcpp_action::Server<Primenumber>::SharedPtr server_;
 
-  rclcpp_action::Goalresponse handle_goal(
-    const rclcpp_action::GoalUUID &,
-    std::shared_ptr<const Primenumber::Goal> goal)
-    {
-        RCLCPP_INFO(this->get_logger(), "Received goal: target=%d", goal->target);
-        if (goal->target <2) {
-            RCLCPP_WARN(this->get_looger(), "target < 2: reject");
-            return rclcpp_action::GoalResponse::REJECT;
-        }
-        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  rclcpp_action::GoalResponse handle_goal(
+      const rclcpp_action::GoalUUID &,
+      std::shared_ptr<const Primenumber::Goal> goal)
+  {
+    RCLCPP_INFO(this->get_logger(), "Received goal: target=%d", goal->target);
+    if (goal->target < 2) {
+      RCLCPP_WARN(this->get_logger(), "target < 2: reject");
+      return rclcpp_action::GoalResponse::REJECT;
     }
-    
+    return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+  }
+
   rclcpp_action::CancelResponse handle_cancel(
-    const std::shared_ptr<GoalHandlePrimenumber>  /*goal_handle*/)
-    {
-        RCLCPP_INFO(this->get_logger(), "Cancel request received");
-        return rclcpp_action::CancelResponse::ACCEPT;
-    }
+      const std::shared_ptr<GoalHandlePrimenumber> /*goal_handle*/)
+  {
+    RCLCPP_INFO(this->get_logger(), "Cancel request received");
+    return rclcpp_action::CancelResponse::ACCEPT;
+  }
 
   void handle_accepted(const std::shared_ptr<GoalHandlePrimenumber> goal_handle)
   {
+    // 실행은 별도 스레드에서
     std::thread{std::bind(&PrimenumberActionServer::execute, this, goal_handle)}.detach();
   }
 
-  void execute(const std::shard_ptr<GoalHandlePrimenumber> goal_handle)
+  void execute(const std::shared_ptr<GoalHandlePrimenumber> goal_handle)
   {
     const auto goal = goal_handle->get_goal();
-    auto result = std::make_shared<PrimenumberAction::Result>();
+    auto result = std::make_shared<Primenumber::Result>();
 
-    for (int i=2; i<= goal->target; ++i) {
-        if (goal_handle->is_canceling()) {
-            RCLCPP_INFO(this->get_logger(), "Goal canceled");
-            goal_handle->canceled(result);
-            return;
-        }
-        bool is_prime=true;
-        for (int j=2;j*j<=i; ++j) {
-            if (i%j==0) (is_prime=false; break;)
-        }
+    for (int i = 2; i <= goal->target; ++i) {
+      if (goal_handle->is_canceling()) {
+        RCLCPP_INFO(this->get_logger(), "Goal canceled");
+        goal_handle->canceled(result);
+        return;
+      }
 
-        if (is_prime) {
-            result->sequence.push_back(i);
-            auto feedback = std::make_shared<Primenumber::Feedback>();
-            feedback->cur_num=i;
-            goal_handle->publish_feedback(feedback);
-            RCLCPP_INFO(this->get_logger(), "Prime: %d", i);
-        }
+      bool is_prime = true;
+      for (int j = 2; j * j <= i; ++j) {
+        if (i % j == 0) { is_prime = false; break; }
+      }
 
-        std::this_thread::sleep_for(100ms);
+      if (is_prime) {
+        result->sequence.push_back(i);
+        auto feedback = std::make_shared<Primenumber::Feedback>();
+        feedback->cur_num = i;
+        goal_handle->publish_feedback(feedback);
+        RCLCPP_INFO(this->get_logger(), "Prime: %d", i);
+      }
+
+      std::this_thread::sleep_for(100ms);
     }
 
     if (rclcpp::ok()) {
-        goal_handle->succeed(result);
-        RCLCPP_INFO(this->get_logger(), "Goal succeeded (found %zu prime).",
-                    result->sequence.size());
+      goal_handle->succeed(result);
+      RCLCPP_INFO(this->get_logger(), "Goal succeeded (found %zu primes).",
+                  result->sequence.size());
     }
   }
 };
 
 int main(int argc, char ** argv)
 {
-    rclcpp::init(argc, argv);
-    rclcpp::spin(std::make_shared<PrimenumberActionServer>());
-    rclcpp::shutdown();
-    return 0;
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<PrimenumberActionServer>());
+  rclcpp::shutdown();
+  return 0;
 }
